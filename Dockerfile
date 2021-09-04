fde2c4967ebdd4f3127f7a04378ac2f013252cfb
Rodrigo Fernandes <rodrigoforit@gmail.com>
2021-09-04 10:53:08 -0300

    Troubleshooting native image error on create r2dbc connection

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
-FROM quay.io/quarkus/ubi-quarkus-native-image:21.2.0-java11 as build
+FROM quay.io/quarkus/ubi-quarkus-native-image:21.1-java11 as build
 WORKDIR /usr/src/app
 COPY . /usr/src/app/
 USER root
 RUN ./gradlew clean
 RUN chown -R quarkus /usr/src/app
 USER quarkus
-RUN ./gradlew clean build -Dquarkus.package.type=native
+RUN ./gradlew clean build -Dquarkus.package.type=native -x test
 
 FROM quay.io/quarkus/quarkus-distroless-image:1.0
 
-COPY --from=build /usr/src/app/build/*-runner /books
+WORKDIR /usr/src/app
+
+COPY --from=build /usr/src/app/build/*-runner /usr/src/app/books
 
 USER nonroot
 
-CMD ./books -Dquarkus.http.host=0.0.0.0
+CMD ["./books", "-Dquarkus.http.host=0.0.0.0"]
--- src/main/java/br/com/code/challenge/book/config/OracleR2dbcConfig.java
+++ src/main/java/br/com/code/challenge/book/config/OracleR2dbcConfig.java
@@ -4,17 +4,40 @@ import io.quarkus.arc.profile.UnlessBuildProfile;
 import io.r2dbc.pool.ConnectionPool;
 import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactoryOptions;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import java.time.Duration;
 
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
 public class OracleR2dbcConfig {
 
   @Inject
   @ConfigProperty(name = "r2dbc.url")
   String url;
+
+  @Inject
+  @ConfigProperty(name = "r2dbc.host")
+  String host;
+
+  @Inject
+  @ConfigProperty(name = "r2dbc.port")
+  String port;
+
+  @Inject
+  @ConfigProperty(name = "r2dbc.database")
+  String database;
+
+  @Inject
+  @ConfigProperty(name = "r2dbc.username")
+  String username;
+
+  @Inject
+  @ConfigProperty(name = "r2dbc.password")
+  String password;
 
   @Inject
   @ConfigProperty(name = "r2dbc.max-size")
@@ -35,7 +58,16 @@ public class OracleR2dbcConfig {
   @Produces
   @UnlessBuildProfile("test")
   ConnectionPool connectionPool() {
-    final var factory = ConnectionFactories.get(url);
+    final var factoryOptions = ConnectionFactoryOptions.builder()
+        .option(DRIVER, "oracle")
+        .option(HOST, host)
+        .option(PORT, Integer.valueOf(port))
+        .option(DATABASE, database)
+        .option(USER, username)
+        .option(PASSWORD, password)
+        .build();
+
+    final var factory = ConnectionFactories.get(factoryOptions);
 
     final var pollConfiguration = ConnectionPoolConfiguration.builder()
         .connectionFactory(factory)
